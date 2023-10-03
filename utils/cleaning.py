b89bffcd8795191bc973457f55f884a534ba0bc3
Pranjal <epe18029@rgipt.ac.in>
2023-10-03 16:17:52 +0530

    cleaning func addition

---
+++ utils/cleaning.py
@@ -0,0 +1,14 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('punkt')
+import re
+def cleaning(stories):
+    cleaned_stories= []
+    for line in stories:
+        line= line.lower()
+        line = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-\\]", "", line) # replacing any special characters in thr line
+        tokens = word_tokenize(line)
+        words = [word for word in tokens if word.isalpha()]
+        cleaned_stories+=words
+    return cleaned_stories
